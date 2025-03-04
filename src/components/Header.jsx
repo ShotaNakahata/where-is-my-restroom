38a7a3938b2553a36ccde1a7b3a6c353b568894e
中畠昇汰 <pandora.osk@gmail.com>
2025-03-04 17:09:04 +0900

    complete nav open and close

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.css";
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
@@ -11,11 +13,18 @@ function Header() {
         </Link>
       </div>
       <div className={styles.menuIcon}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={styles.icon}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />Ï
-        </svg>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {!isOpen ?
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon}`}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />Ï
+            </svg> :
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon}`}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+          }
+        </button>
       </div>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
         <ul className={`${styles.navList} link`}>
           {/* stateとlogin機能を準備したらWelcome Guestを書き換える "Rewrite 'Welcome Guest' after preparing state and login functionality."*/}
           <li><Link href="/map" className={styles.navLink}>Welcome Guest</Link></li>{/* ！！！！！ */}
