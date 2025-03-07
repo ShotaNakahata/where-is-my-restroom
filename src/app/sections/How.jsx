21fdba327f1328be2d86023833c0681e960c1de7
中畠昇汰 <pandora.osk@gmail.com>
2025-03-07 10:47:13 +0800

    Temporary completion of section how structure and design

--- src/app/sections/How.jsx
+++ src/app/sections/How.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 import Image from "next/image";
 import styles from "./How.module.css";
 
+const steps = [
+  {
+    id: 1,
+    imgSrc: "/app-screen-1.png",
+    title: "Tell us what you like (and what not)",
+    description:
+      "Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!",
+  },
+  {
+    id: 2,
+    imgSrc: "/app-screen-1.png",
+    title: "Tell us what you like (and what not)",
+    description:
+      "Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!",
+  },
+  {
+    id: 3,
+    imgSrc: "/app-screen-1.png",
+    title: "Tell us what you like (and what not)",
+    description:
+      "Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!",
+  },
+  {
+    id: 4,
+    imgSrc: "/app-screen-1.png",
+    title: "Tell us what you like (and what not)",
+    description:
+      "Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!",
+  },
+]
 export default function How() {
   return (
     <section className={styles.sectionHow}>
@@ -11,26 +41,21 @@ export default function How() {
           Your daily dose of health in 3 simple steps
         </h2>
       </div>
-
-      <div className={`container`}>
-        
-        <div className={styles.featureImgBox}>
-          <Image src="/app-screen-1.png" width="100" height="200" alt="iPhone app
-            preferences selection screen"/>
-        </div>
-
-        <div className={styles.featureTextBox}>
-          <p className={styles.featureNum}>01</p>
-          <h3 className={styles.featureTertiary}>
-            Tell us what you like (and what not)
-          </h3>
-          <p className={styles.featureDesc}>
-            Never again waste time thinking about what to eat! Omnifood AI
-            will create a 100% personalized weekly meal plan just for you. It
-            makes sure you get all the nutrients and vitamins you need, no
-            matter what diet you follow!
-          </p>
-        </div>
+      <div className={`container grid grid1col`}>
+        {steps.map((step) => {
+          return (
+            <div key={step.id} className={styles.stepContainer}>
+              <div className={styles.featureImgBox}>
+                <Image src={step.imgSrc} width="100" height="200" alt={step.title} />
+              </div>
+              <div className={styles.featureTextBox}>
+                <p className={styles.featureNum}>{`0${step.id}`}</p>
+                <h3 className={styles.featureTertiary}>{step.title}</h3>
+                <p className={styles.featureDesc}>{step.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
