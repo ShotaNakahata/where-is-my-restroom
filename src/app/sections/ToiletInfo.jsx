c0dabeb24b45dd210fc7d12c0e1bda8881307510
中畠昇汰 <pandora.osk@gmail.com>
2025-03-07 13:04:28 +0800

    Completed version 1 of the toilet-info section with basic design and structure

--- src/app/sections/ToiletInfo.jsx
+++ src/app/sections/ToiletInfo.jsx
@@ -30,7 +30,7 @@ function ToiletInfo() {
       <div className={`container grid grid1col`}>
         {ToietInfoEx.map((toiletEx) => {
           return (
-            <div key={toiletEx.id} className={styles.toilet}>
+            <div key={toiletEx.id} className={`${styles.toilet} box`}>
               <div className={toiletEx.toiletImg}>
                 <Image src={toiletEx.imgSrc} alt={toiletEx.alt} width="350" height="233" />
               </div>
@@ -52,7 +52,7 @@ function ToiletInfo() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                     </svg>
-                    <span>{toiletEx.place}</span>
+                    <span>{toiletEx.rating}</span>
                   </li>
                 </ul>
               </div>
