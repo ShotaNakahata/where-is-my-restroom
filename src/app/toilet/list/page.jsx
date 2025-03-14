a7f1ea5046c35469ee662ec8d584518c1010d6ca
中畠昇汰 <pandora.osk@gmail.com>
2025-03-14 20:42:31 +0800

    complete  list page toilet content ui

--- src/app/toilet/list/page.jsx
+++ src/app/toilet/list/page.jsx
@@ -21,27 +21,29 @@ function page() {
           return (
             <div key={toilet.id} className={`${styles.toilet} box`}>
               <div className={styles.toiletImg}>
-                <Image className={styles.img} src={toilet.imgSrc} alt="" width="100" height="100"style={{ width: "100%", height: "auto" }} />
+                <Image className={styles.img} src={toilet.imgSrc} alt="" width="100" height="100" style={{ width: "100%", height: "auto" }} />
               </div>
               <div className={styles.toiletTextInfo}>
-                <span
-                  className={`${styles.toiletType} ${toilet.accessible ? styles.accessible : styles.notAccessible}`}>
-                  {toilet.accessible ? "accessible" : "NOT accessible"}
-                </span>
-                <p className={styles.toiletName}>{toilet.name}</p>
+                <div className={styles.mainTileBox}>
+                  <span
+                    className={`${styles.toiletType} ${toilet.accessible ? styles.accessible : styles.notAccessible}`}>
+                    {toilet.accessible ? "accessible" : "NOT accessible"}
+                  </span>
+                  <p className={styles.toiletName}>{toilet.name}</p>
+                </div>
                 <ul className={styles.cardUl}>
-                  <li className={styles.carli}>
+                  <li className={styles.cardli}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                       <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                     </svg>
-                    <span>{toilet.address}</span>
+                    <span className={styles.span}>{toilet.address}</span>
                   </li>
-                  <li className={styles.carli}>
+                  <li className={styles.cardli}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                     </svg>
-                    <span>{toilet.rating}</span>
+                    <span className={styles.span}>{toilet.rating}</span>
                   </li>
                 </ul>
               </div>
