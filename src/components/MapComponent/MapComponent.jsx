fd5887bf1dd049db9855d2c5af71eb696334db19
中畠昇汰 <pandora.osk@gmail.com>
2025-03-19 04:51:21 +0800

    prepare google map api

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,7 +1,14 @@
 "use client"
 import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import { fetchToilets } from "@/lib/fetchToilets";
 
 function MapComponent() {
+  const { data: toilets } = useQuery({
+    queryKey: ["toilets"],
+    queryFn: fetchToilets,
+    staleTime: 1000 * 60 * 5,
+  });
   return (
     <div>MapComponent</div>
   )
