944dfe45dfacce5d3cb8d7453c0d35b51ac679c7
中畠昇汰 <pandora.osk@gmail.com>
2025-03-22 10:58:52 +0800

    Prepare Redux setup to store favorite toilets for real-time UI updates

---
+++ src/redux/slices/favoriteSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favoriteToilets: []
+}
+
+const favoriteSlice = createSlice({
+  name: "favorite",
+  initialState,
+  reducers: {
+    setFavorites: (state, action) => {
+      state.favoriteToilets = action.payload;
+    },
+    addFavorite: (state, action) => {
+      if (!state.favoriteToilets.find(toilet => toilet._id === action.payload._id)) {
+        state.favoriteToilets.push(action.payload);
+      }
+    },
+    removeFavorite: (state, action) => {
+      state.favoriteToilets = state.favoriteToilets.filter(t => t._id !== action.payload);
+    },    
+    clearFavorites: (state) => {
+      state.favoriteToilets = []
+    }
+  }
+});
+
+export const { setFavorites, addFavorite, removeFavorite, clearFavorites } = favoriteSlice.actions;
+export default favoriteSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 "use client"; 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/redux/slices/authSlice";
+import favoriteReducer from "@/redux/slices/favoriteSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    favorite:favoriteReducer
   }
 })
 
