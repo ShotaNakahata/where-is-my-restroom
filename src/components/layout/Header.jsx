93bd0dc9209d7b1de0ba337d5e696564a0f1ae70
中畠昇汰 <pandora.osk@gmail.com>
2025-03-28 15:02:43 +0800

    fix: adjust mobile menu icon positioning

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -53,7 +53,7 @@ function Header() {
         </Link>
       </div>
       <div className={`${styles.menuIcon}`}>
-        <button className={styles.hederBtn} onClick={() => setIsOpen(!isOpen)}>
+        {/* <button className={styles.hederBtn} onClick={() => setIsOpen(!isOpen)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -70,7 +70,24 @@ function Header() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             )}
           </svg>
+        </button> */}
+        <button className={styles.hederBtn} onClick={() => setIsOpen(!isOpen)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={styles.icon}
+          >
+            {isOpen ? (
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            ) : (
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            )}
+          </svg>
         </button>
+
 
       </div>
       <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
