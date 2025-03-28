5b17c17a644aff6ba53022d72585df0f223af970
中畠昇汰 <pandora.osk@gmail.com>
2025-03-28 11:46:48 +0800

    fix: header menu icon position on close

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,78 @@
+// "use client"
+// import React, { useState } from 'react'
+// import Link from "next/link";
+// import Image from "next/image";
+// import styles from "@/components/layout/Header.module.css";
+// import { useRefContext } from '@/providers/RefContext';
+// import { scrollToRef } from "@/utils/scrollUtils";
+// import { useSelector, useDispatch } from "react-redux";
+// import { logout } from "@/redux/slices/authSlice";
+// import { clearFavorites } from "@/redux/slices/favoriteSlice";
+// import { usePathname } from "next/navigation";
+// import LoginModal from "@/components/common/LoginModal";
+
+// function Header() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+//   const { loginRef } = useRefContext();
+//   const { user, isAuthenticated } = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+//   const pathName = usePathname()
+
+//   function handleLoginClick() {
+//     if(pathName==="/"){
+//       scrollToRef(loginRef)
+//     }else{
+//       setIsLoginModalOpen(true);
+//     }
+//   }
+//   function handleLogout() {
+//     dispatch(logout());
+//     dispatch(clearFavorites());
+//     localStorage.removeItem("user");
+//   }
+
+//   return (
+//     <header className={styles.header}>
+//       {isLoginModalOpen && <LoginModal onCloseIsModal={() => setIsLoginModalOpen(false)} />}
+//       <div className={styles.logoContainer}>
+//         <Link href="#">
+//           <Image className={styles.logo} src="/logo.webp" alt='Logo img' width={90} height={68} />
+//         </Link>
+//       </div>
+//       <div className={`${styles.menuIcon}`}>
+//         <button className={`${styles.hederBtn} `} onClick={() => setIsOpen(!isOpen)}>
+//           {!isOpen ?
+//             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon}`}>
+//               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+//             </svg> :
+//             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon} ${styles.fixedIcon}`}>
+//               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+//             </svg>
+//           }
+//         </button>
+//       </div>
+//       <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
+//         <ul className={`${styles.navList} link`}>
+//           <li className={`${styles.Welcome}`}>
+//             <Link href="/#" className={`${styles.navLink} ${styles.Welcome}`}>Welcome {!isAuthenticated ? "Guest" : user.name}</Link>
+//           </li>
+//           <li><Link href="/" className={styles.navLink}>Home</Link></li>
+//           <li><Link href="/map" className={styles.navLink}>Map</Link></li>
+//           <li><Link href="/contact" className={styles.navLink}>Contact</Link></li>
+//           <li><Link href="/mypage" className={styles.navLink}>MyPage</Link></li>
+//           <li>
+//             {!isAuthenticated ? 
+//             <a onClick={handleLoginClick} className={`${styles.navLink} btnLg ${styles.login}`}>Login</a> 
+//             : <a onClick={handleLogout} className={`${styles.navLink} btnLg ${styles.login}`}>Logout</a>}
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   )
+// }
+
+// export default Header
 "use client"
 import React, { useState } from 'react'
 import Link from "next/link";
@@ -17,15 +92,16 @@ function Header() {
   const { loginRef } = useRefContext();
   const { user, isAuthenticated } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const pathName = usePathname()
+  const pathName = usePathname();
 
   function handleLoginClick() {
-    if(pathName==="/"){
-      scrollToRef(loginRef)
-    }else{
+    if (pathName === "/") {
+      scrollToRef(loginRef);
+    } else {
       setIsLoginModalOpen(true);
     }
   }
+
   function handleLogout() {
     dispatch(logout());
     dispatch(clearFavorites());
@@ -40,36 +116,48 @@ function Header() {
           <Image className={styles.logo} src="/logo.webp" alt='Logo img' width={90} height={68} />
         </Link>
       </div>
-      <div className={`${styles.menuIcon}`}>
-        <button className={`${styles.hederBtn} `} onClick={() => setIsOpen(!isOpen)}>
-          {!isOpen ?
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon}`}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg> :
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${styles.icon} ${styles.fixedIcon}`}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          }
+      <div className={styles.menuIcon}>
+        <button className={styles.hederBtn} onClick={() => setIsOpen(!isOpen)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={`${styles.icon} ${isOpen ? styles.fixedIcon : ""}`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d={
+                isOpen
+                  ? "M6 18 18 6M6 6l12 12" // ×アイコン
+                  : "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" // ≡アイコン
+              }
+            />
+          </svg>
         </button>
       </div>
       <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
         <ul className={`${styles.navList} link`}>
-          <li className={`${styles.Welcome}`}>
-            <Link href="/#" className={`${styles.navLink} ${styles.Welcome}`}>Welcome {!isAuthenticated ? "Guest" : user.name}</Link>
+          <li className={styles.Welcome}>
+            <Link href="/#" className={`${styles.navLink} ${styles.Welcome}`}>
+              Welcome {!isAuthenticated ? "Guest" : user.name}
+            </Link>
           </li>
           <li><Link href="/" className={styles.navLink}>Home</Link></li>
           <li><Link href="/map" className={styles.navLink}>Map</Link></li>
           <li><Link href="/contact" className={styles.navLink}>Contact</Link></li>
           <li><Link href="/mypage" className={styles.navLink}>MyPage</Link></li>
           <li>
-            {!isAuthenticated ? 
-            <a onClick={handleLoginClick} className={`${styles.navLink} btnLg ${styles.login}`}>Login</a> 
-            : <a onClick={handleLogout} className={`${styles.navLink} btnLg ${styles.login}`}>Logout</a>}
+            {!isAuthenticated ?
+              <a onClick={handleLoginClick} className={`${styles.navLink} btnLg ${styles.login}`}>Login</a>
+              : <a onClick={handleLogout} className={`${styles.navLink} btnLg ${styles.login}`}>Logout</a>}
           </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
