bd98360605a0d5bc396b3bf1e0e3cd3170060949
中畠昇汰 <pandora.osk@gmail.com>
2025-03-31 16:23:52 +0800

    💄 Fix: Update UI for filter component on ListPage

--- src/app/toilet/list/page.jsx
+++ src/app/toilet/list/page.jsx
@@ -1,32 +1,3 @@
-// import { dehydrate, QueryClient } from "@tanstack/react-query";
-// import { HydrationBoundary } from "@tanstack/react-query";
-// import ListPageClient from "@/components/toilet/ListPageClient";
-// import { fetchToilets } from "@/lib/fetchToilets";
-
-// const LIMIT = 12;
-
-// export default async function ToiletListPage() {
-//   const queryClient = new QueryClient();
-
-//   // ✅ 無限スクロール用のプリフェッチ（最初の1ページ）
-//   await queryClient.prefetchInfiniteQuery({
-//     queryKey: ["toilets"],
-//     queryFn: ({ pageParam = 0 }) => fetchToilets({ limit: LIMIT, offset: pageParam }),
-//     getNextPageParam: (lastPage, allPages) => {
-//       const nextOffset = allPages.flat().length;
-//       return lastPage.length < LIMIT ? undefined : nextOffset;
-//     },
-//   });
-
-//   console.log("🟢 [Server] Prefetched Toilets:", queryClient.getQueryData(["toilets"]));
-
-//   return (
-//     <HydrationBoundary state={dehydrate(queryClient)}>
-//       <ListPageClient />
-//     </HydrationBoundary>
-//   );
-// }
-
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { HydrationBoundary } from "@tanstack/react-query";
 import ListPageClient from "@/components/toilet/ListPageClient";
--- src/lib/fetchToilets.js
+++ src/lib/fetchToilets.js
@@ -1,26 +1,3 @@
-// export async function fetchToilets(id = null) {
-//   console.log("🔍 fetchToilets called with id:", id); // ✅ `id` の値を確認
-
-//   const url = id
-//     ? `${process.env.NEXT_PUBLIC_API_URL}/api/toilets/${id}`
-//     : `${process.env.NEXT_PUBLIC_API_URL}/api/toilets`;
-
-//   console.log("Fetching toilets from API:", url); // ✅ `fetch` の URL を確認
-
-//   const res = await fetch(url, { cache: "no-store" });
-
-//   if (!res.ok) {
-//     console.error("❌ Failed to fetch toilets:", res.status);
-//     throw new Error("Failed to fetch toilets");
-//   }
-
-//   const data = await res.json();
-//   console.log("Fetched Toilets Data:", data); // ✅ `fetch` したデータをログ出力
-//   return data;
-// }
-
-// fetchToilets.ts
-
 
 //fetch用途 1,db全てを取得 2,個数を制限して取得 3該当dataを取得
 export async function fetchToilets({ id = null, limit = null, offset = null } = {}) {
